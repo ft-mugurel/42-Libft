48ee5f6baca39612cf41778898546b4b3340e8bb
Muhammed Talha Ugurel <muhammedtalhaugurel@gmail.com>
2022-12-06 02:37:14 +0300

    some changes

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: mugurel <muhammedtalhaugurel@gmai...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 02:25:36 by mugurel           #+#    #+#             */
-/*   Updated: 2022/12/06 02:26:38 by mugurel          ###   ########.fr       */
+/*   Updated: 2022/12/06 02:33:04 by mugurel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
+	size_t			i;
+	unsigned char	*buc;
 
+	i = 0;
+	buc = (unsigned char *)s;
+	while (i < n)
+	{
+		buc[i] = c;
+		i++;
+	}
+	return (s = buc);
 }
