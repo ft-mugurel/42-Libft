4662249a119f706511eba4ba2736ed2561cb8f80
Muhammed Talha Ugurel <muhammedtalhaugurel@gmail.com>
2022-12-10 17:42:10 +0300

    Second tester chacks 2

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mugurel <muhammedtalhaugurel@gmai...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 02:43:04 by mugurel           #+#    #+#             */
-/*   Updated: 2022/12/09 03:04:50 by mugurel          ###   ########.fr       */
+/*   Updated: 2022/12/10 17:40:41 by mugurel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (s1[i] && s2[i] && i < n)
+	while (i < n && s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[1] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		}
 		i++;
 	}
 	if (i == n)
 		i--;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
